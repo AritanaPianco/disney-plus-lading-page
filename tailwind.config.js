9643b08161f06e5dbe7d930c63680db6007580e7
AritanaPianco <aritanapianco10@gmail.com>
2023-11-22 17:12:25 -0300

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+       'sans': ['Roboto'] 
+    },
+    colors: {
+       background: 'rgb(5, 5, 20)',
+       cinza: '#ececec',
+       azulBnt: 'rgb(45, 96, 185)',
+       azulVioleta: 'rgb(90, 63, 225)',
+       cinzaEscuro: '#cdcdcd',
+       btnBackground: 'rgb(63, 116, 207)',
+       btnBackgroundHover: 'rgb(114, 62, 218)',
+       colorWhite: 'rgb(255 255 255)',
+       backgroundCinza: 'rgb(43, 43, 53)',
+       cinzaLetra: '#d4d4d8'
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
